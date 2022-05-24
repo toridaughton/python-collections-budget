8ca53512dc5ee8b995bed0639927cc16a40532be
Tori Daughton <toriddaughton@gmail.com>
2022-05-23 21:53:27 -0600

    Completed module

--- budget/Expense.py
+++ budget/Expense.py
@@ -43,11 +43,12 @@ class Expenses():
     def categorize_set_comprehension(self):
         necessary_expenses = {
             x for x in self.list
-                if x.category == "Phone" or x.category == "Auto and Gas" or x.category == "Classes" or x.category == "Utilities" or x.category == "Mortgage"
+            
+            if x.category == "Phone" or x.category == "Auto and Gas" or x.category == "Classes" or x.category == "Utilities" or x.category == "Mortgage"
             }
         food_expenses = {
             x for x in self.list
-                if x.category == "Groceries" or "Eating Out"
+            if x.category == "Groceries" or "Eating Out"
         }
         unnecessary_expenses = set(self.list).difference(food_expenses)
         return [necessary_expenses, food_expenses, unnecessary_expenses]
--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -8,8 +8,8 @@ def main():
     divided_set_comp = expenses.categorize_set_comprehension()
     if(divided_set_comp != divided_for_loop):
         print("Sets are NOT equal by == test")
-    for d_loop, set_comp in zip(divided_for_loop, divided_set_comp):
-        if not (d_loop.issubset(set_comp) and set_comp.issubset(d_loop)):
+    for a, b in zip(divided_for_loop, divided_set_comp):
+        if not (a.issubset(b) and a.issubset(b)):
             print("Sets are NOT equal by supbset test")
 
 if __name__ == "__main__":
