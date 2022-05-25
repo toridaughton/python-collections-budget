c0b3c3158374fb2015a6a6319cca6b6d25121dce
Tori Daughton <toriddaughton@gmail.com>
2022-05-24 21:11:26 -0600

    part 2 of module 5 section 6 fix

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -12,16 +12,16 @@ def main():
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
-    print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
+    print(timeit.timeit(stmt =
+"expenses.categorize_for_loop()",
                         setup = 
-                        '''
-                        from . import Expense
-                        expenses = Expense.Expenses()
-                        expenses.read_expenses('data/spending_data.csv')
-                        ''', 
+'''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+''', 
                         number=10_000,
-                        globals=globals())
-    )
+                        globals=globals()))
 
 if __name__ == "__main__":
     main()
